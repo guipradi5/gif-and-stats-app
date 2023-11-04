0934cf701fc26c1215dbb69e4f5ee558eb61bcbc
Guillermo P <guipradi5@gmail.com>
2023-11-04 02:46:00 +0100

    Add gif count api calls

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,22 @@
 import axios from "axios";
 
 export const getGifs = async () => {
-    return (await axios.get("https://g.tenor.com/v1/search?q=excited&key=LIVDSRZULELA&limit=8")).data.results
+    const result = await axios.get("https://g.tenor.com/v1/search?q=excited&key=LIVDSRZULELA&limit=8")
+    return result.data.results
+}
+
+/* 
+Ideally this could have been done with MongoDB or another NoSQL solution to save and 
+get the click count for each gif among all users of the application.
+But I decided to keep it simple for this exercise using localStorage to simulate this.
+Didn't want to over-engineer it for this simple exercise where the FE is the main focus.
+*/
+
+export const getGifClickCount = async (gifId) => {
+    return await localStorage.getItem(gifId)
+}
+
+export const postGifClick = async (gifId) => {
+    const clickCount = await getGifClickCount(gifId) || 0
+    localStorage.setItem(gifId, parseInt(clickCount) + 1);
 }
